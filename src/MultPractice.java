fd5850ce7c0d6370141eb6b6af3327711b02ad2e
BT_1N3_29 <zchen5143@bths.edu>
2019-04-01 08:27:44 -0400

    multpractice frq

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,17 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice{
+    private int firstInt;
+    private int initialSecondInt;
+    public MultPractice(int firstInteger, int initialSecondInteger){
+        firstInt=firstInteger;
+        initialSecondInt= initialSecondInteger;
+    }
+    public String getProblem(){
+        return firstInt + " TIMES "+ initialSecondInt;
+    }
+    public void nextProblem(){
+        initialSecondInt++;
+    }
 }
